1cb6d562e0b2a98cdb282b9f9013310911040045
TurkerCalhan <turker.calhan@outlook.com>
2023-02-28 13:00:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "kVHdFi4l+ERFIJ34i8KYGg==wjgSfPgMIjs2Kcr2";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText = "An error happened, try again later";
+    console.log(error);
+  }
+}
+
+btnEl.addEventListener("click", getJoke);
